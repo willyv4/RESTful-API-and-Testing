7a305636c10ca9bd0a5bd1ac106e13086acc6517
William Valadez <billy.valadez@icloud.com>
2023-03-17 23:52:14 -0400

    added code to tests.py

--- TodoAPI/tests.py
+++ TodoAPI/tests.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_ECHO'] = False
 # Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
-
+db.drop_all()
 db.create_all()
 
 
